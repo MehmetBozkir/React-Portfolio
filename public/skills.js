47a701e6a2ec5dfb15000a1a862846848edfee86
Mehmet Salih Bozkır <150898451+MehmetBozkir@users.noreply.github.com>
2024-04-10 03:12:59 +0300

    Create Website

---
+++ public/skills.js
@@ -0,0 +1,16 @@
+export const skillsData = [
+  "HTML",
+  "CSS",
+  "Javascript",
+  "React",
+  "Typescript",
+  "Vitejs",
+  "Next JS",
+  "Tailwind",
+  "Git",
+  "Bootstrap",
+  "Figma",
+  "Firebase",
+  "MaterialUI",
+  "Strapi",
+];
