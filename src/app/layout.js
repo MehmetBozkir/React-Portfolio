ef6a69e6fafc665afc372fdd9decaf8ed5911184
Mehmet Salih Bozkır <150898451+MehmetBozkir@users.noreply.github.com>
2024-04-25 18:26:33 +0300

    Design Update Vol2

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import TransitionProvider from "@/components/transitionProvider";
+import StarsCanvas from "@/components/Star";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,7 +14,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <TransitionProvider>{children}</TransitionProvider>
+        <TransitionProvider>
+          <StarsCanvas />
+          {children}
+        </TransitionProvider>
       </body>
     </html>
   );
---
+++ src/components/Social.jsx
@@ -0,0 +1,53 @@
+"use client";
+
+import React from "react";
+import Link from "next/link";
+import { FaGithub } from "react-icons/fa";
+import { FaMedium } from "react-icons/fa6";
+import { IoLogoStackoverflow } from "react-icons/io5";
+import { FaLinkedin } from "react-icons/fa";
+import { SiLinktree } from "react-icons/si";
+
+function Social() {
+  return (
+    <div className="flex gap-4 bg-white p-2 rounded-full bg-opacity-40">
+      <Link
+        href="https://github.com/MehmetBozkir"
+        target="_blank"
+        aria-label="Github Link"
+      >
+        <FaGithub className="text-4xl hover:scale-[1.35]" />
+      </Link>
+      <Link
+        href="https://medium.com/@mehmetsalihbozkir"
+        target="_blank"
+        aria-label="Medium Link"
+      >
+        <FaMedium className="text-4xl hover:scale-[1.35]" />
+      </Link>
+      <Link
+        href="https://stackoverflow.com/users/23094164/mehmet-salih-bozk%c4%b1r"
+        target="_blank"
+        aria-label="StackoverFlow Link"
+      >
+        <IoLogoStackoverflow className="text-4xl hover:scale-[1.35]" />
+      </Link>
+      <Link
+        href="https://www.linkedin.com/in/mehmet-salih-bozk%C4%B1r/"
+        target="_blank"
+        aria-label="Linkedin Link"
+      >
+        <FaLinkedin className="text-4xl hover:scale-[1.35]" />
+      </Link>
+      <Link
+        href="https://linktr.ee/MehmetSalihBozkir"
+        target="_blank"
+        aria-label="Linktr Link"
+      >
+        <SiLinktree className="text-4xl hover:scale-[1.35]" />
+      </Link>
+    </div>
+  );
+}
+
+export default Social;
