bca292ca23d8292b26f5069e459c3fc976fc99c9
Mehmet Salih Bozkır <150898451+MehmetBozkir@users.noreply.github.com>
2024-04-26 16:46:51 +0300

    Update New Cv

--- src/components/Transmission.jsx
+++ src/components/Transmission.jsx
@@ -8,8 +8,8 @@ function Transmission() {
   return (
     <div className="flex gap-4">
       <a
-        href={"indir.pdf"}
-        download={"Cw.pdf"}
+        href={"MehmetSalihBozkir_Cv.pdf"}
+        download={"MehmetSalihBozkir_Cv.pdf"}
         className="p-4 rounded-full ring-1 ring-black bg-black text-white bg-opacity-80 flex hover:scale-[1.15]"
       >
         Download CV <FiDownload className="text-2xl ml-2" />
