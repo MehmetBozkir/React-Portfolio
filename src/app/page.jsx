7d4e1730e6bd8ed0537715129ecf932e486bc51a
Mehmet Salih Bozkır <150898451+MehmetBozkir@users.noreply.github.com>
2024-04-26 23:02:00 +0300

    Update Responsive

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -15,14 +15,14 @@ const Homepage = () => {
       transition={{ duration: 1 }}
     >
       <Cnsole />
-      <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+      <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 p-20">
         <div className="h-1/2 lg:h-full lg:w-5/12 flex justify-center items-center">
           <div className="deneme"></div>
         </div>
         <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center text-white">
           <h1 className="text-4xl md:text-6xl font-bold">
             Brushstrokes on a Digital Canvas: <br />
-            <span className="md:text-5xl ">
+            <span className="md:text-5xl">
               <Animation />
             </span>
           </h1>
